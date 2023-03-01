98d19b547cace5d64e394099eb3d5683b3cbe90c
LoicColomoDev <loic.colomo2022@campus-eni.fr>
2023-03-01 16:01:50 +0100

    Base de la page principale non terminée, mise au point navbar et autre details

--- src/Form/FormFiltreSortiesType.php
+++ src/Form/FormFiltreSortiesType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Campus;
 use App\Entity\Sortie;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,8 +20,20 @@ class FormFiltreSortiesType extends AbstractType
             ->add('dateHeureDebut')
             ->add('dateLimiteInscription')
             ->add('organisateur')
+            ->add('nom', TextType::class, [
+                'label' => 'Nom du campus',
+                'attr' => [
+                    'placeholder' => 'Le nom contient...',
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('Rechercher', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary'
+                ]
+            ]);
 
-        ;
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
